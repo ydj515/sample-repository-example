69fc5daf0ba6b93a75d9063621690d9dbff02f86
ydj515 <ydj515@hanmail.net>
2025-07-28 02:05:32 +0900

    fix: adjust pr review in local cache example

--- local-cache-example/src/main/kotlin/com/example/localcacheexample/config/CacheConfig.kt
+++ local-cache-example/src/main/kotlin/com/example/localcacheexample/config/CacheConfig.kt
@@ -17,21 +17,20 @@ class CacheConfig {
 
     companion object {
         private val log = LoggerFactory.getLogger(CacheConfig::class.java)
+        private val DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     }
 
     // refreshAfterWrite는 LoadingCache으로 사용해야 사용 가능
     // refreshAfterWrite(1, TimeUnit.MINUTES) // 1분마다 refresh
     @Bean
     fun caffeineConfig(): Caffeine<Any, Any> {
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
         return Caffeine.newBuilder()
             .expireAfterWrite(3, TimeUnit.MINUTES) // 3분 후 만료
             .expireAfterAccess(5, TimeUnit.MINUTES) // 5분 미사용 시 만료
             .maximumSize(1000000)
             .recordStats()
             .removalListener { key, value, cause ->
-                val now = LocalDateTime.now().format(formatter)
+                val now = LocalDateTime.now().format(DATE_TIME_FORMATTER)
                 log.info("[{}] 캐시 제거됨 - key: {}, cause: {}", now, key, cause)
             }
     }
