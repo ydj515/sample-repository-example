5ad6f94d14af31a282cbecd535b6f4ca43b9d264
ydj515 <ydj515@hanmail.net>
2025-07-27 23:55:36 +0900

    feat: add stage comment int load-test-example

--- load-test-example/k6/script.js
+++ load-test-example/k6/script.js
@@ -1,11 +1,17 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, sleep } from "k6";
 
 export const options = {
   vus: 100,
   duration: "1m"
+  //   stages: [
+  //     { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+  //     { duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
+  //     { duration: '5m', target: 0 }, // ramp-down to 0 users
+  // ]
 };
 
 export default function () {
   http.get("http://localhost:8080");
+  // sleep(1); // 각 요청 사이 1초 대기 넣을까?
 }
